e2439e9766b13c1721dd313f13b6dcc48a2526e2
Narwar <85039332+Narwar@users.noreply.github.com>
2024-07-12 06:28:02 +0000

    Class 5 start

---
+++ Coding_Problem_DSA/Class_5/Program_12.cpp
@@ -0,0 +1,25 @@
+//  Generate all  string with equal no. of "a" and "b";
+
+
+#include<iostream>
+using namespace std;
+ 
+ Generate(string abtk,int first, int second){
+
+
+
+ }
+
+
+
+int main(){
+
+    int n;
+    cout<<"Enter a number : "<<endl;
+    cin>>n;
+    Generatr("",n,n);
+
+
+
+    return 0;
+}
