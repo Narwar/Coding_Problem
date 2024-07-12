8edc8c850a142241f482b0f56149fa7eff18fa0d
Narwar <85039332+Narwar@users.noreply.github.com>
2024-07-12 06:04:50 +0000

    Class 4 done (complete)

--- Coding_Problem_DSA/Class_4/Program_10.cpp
+++ Coding_Problem_DSA/Class_4/Program_10.cpp
@@ -1,20 +1,29 @@
 //  Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
  
- #include<iostrem>
- using namespace std
+ #include<iostream>
+ #include<string>
+ using namespace std;
+ 
 
-   void Generate(){
+   void Generate(string abtk,int open ,int close ){
+     if(open==0 && close==0){
+        cout<<abtk<<endl;
+     }
+     if(open>0){
+        Generate(abtk+"(",open-1,close);
 
-    
+     }
+     if(close>0&& open<close){
+        Generate(abtk+")",open,close-1);
+     }
+
    }
-
-
-
  int main(){
-
-
-
+    int n;
+    cout<<"Enter a no.: "<<endl;
+    cin>>n;
+    Generate("",n,n);
 
     return 0;
 
